be088066004baa7914adbf900bc7106afa4c01c0
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-05-30 14:39:59 +0200

    Modified face for console use.

--- site-start.d/99custom.el
+++ site-start.d/99custom.el
@@ -1,16 +1,23 @@
+;;(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+;; '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+;; '(font-lock-comment-face ((t (:foreground "red"))) nil "Highlight comments in source code correctly")
+;; '(fringe ((((class color) (background light)) (:background "grey60" :foreground "white"))))
+;; '(mode-line ((((class color) (min-colors 88)) (:background "black" :foreground "white" :box (:line-width -1 :style released-button)))))
+;; '(mode-line-inactive ((default (:inherit mode-line)) (((class color) (min-colors 88) (background light)) (:background "grey60" :foreground "white" :box (:line-width -1 :color "grey75") :weight light))))
+;; '(org-level-1 ((t (:inherit outline-1 :foreground "black" :weight bold :height 1.2 :family "arial"))))
+;; '(org-level-2 ((t (:inherit outline-2 :foreground "#151B8D" :weight ultra-bold :height 1.2))))
+;; '(org-level-3 ((t (:inherit outline-3 :foreground "orange" :underline t :weight bold)))))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
  '(font-lock-comment-face ((t (:foreground "red"))) nil "Highlight comments in source code correctly")
- '(fringe ((((class color) (background light)) (:background "grey60" :foreground "white"))))
- '(mode-line ((((class color) (min-colors 88)) (:background "black" :foreground "white" :box (:line-width -1 :style released-button)))))
- '(mode-line-inactive ((default (:inherit mode-line)) (((class color) (min-colors 88) (background light)) (:background "grey60" :foreground "white" :box (:line-width -1 :color "grey75") :weight light))))
- '(org-level-1 ((t (:inherit outline-1 :foreground "black" :weight bold :height 1.3 :family "arial"))))
- '(org-level-2 ((t (:inherit outline-2 :foreground "#151B8D" :weight ultra-bold :height 1.2))))
- '(org-level-3 ((t (:inherit outline-3 :foreground "orange" :underline t :weight bold)))))
+)
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
