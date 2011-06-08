e6ee3a824a7b25182dd997b525ac9f91941bf741
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-06-08 11:08:56 +0200

    Made xpdf suitable for console use.

--- site-start.d/50modes.el
+++ site-start.d/50modes.el
@@ -3,10 +3,59 @@
 
 (setq TeX-view-program-list (quote (("xpdf-display" "DISPLAY=:0 xpdf %o")
                                     ("xdvi-display" "DISPLAY=:0 xdvi %o"))))
-(setq TeX-view-program-selection '(((output-dvi style-pstricks) "dvips and gv")
-                                   (output-dvi "xdvi-display")
-                                   (output-pdf "xpdf-display")
-                                   (output-html "xdg-open")))
+(setq TeX-view-program-selection (cond
+                                  ((eq system-type 'windows-nt)
+                                   '(((output-dvi style-pstricks)
+                                      "dvips and start")
+                                     (output-dvi "Yap")
+                                     (output-pdf "start")
+                                     (output-html "start")))
+                                  (t
+                                   '(((output-dvi style-pstricks)
+                                      "dvips and gv")
+                                     (output-dvi "xdvi")
+                                     (output-pdf "xpdf-display")
+                                     (output-html "xdg-open")))))
+
+(setq TeX-command-list
+      `(("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+         (plain-tex-mode ams-tex-mode texinfo-mode)
+         :help "Run plain TeX")
+        ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
+         (latex-mode doctex-mode)
+         :help "Run LaTeX")
+        ("Makeinfo" "makeinfo %t" TeX-run-compile nil
+         (texinfo-mode)
+         :help "Run Makeinfo with Info output")
+        ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil
+         (texinfo-mode)
+         :help "Run Makeinfo with HTML output")
+        ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
+         (ams-tex-mode)
+         :help "Run AMSTeX")
+        ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil
+         (context-mode)
+         :help "Run ConTeXt once")
+        ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil
+         (context-mode)
+         :help "Run ConTeXt until completion")
+        ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
+;        ,(if
+;             (or window-system
+;                 (getenv "DISPLAY"))
+        ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
+;           '("View" "dvi2tty -q -w 132 %s" TeX-run-command t t :help "Run Text viewer"))
+        ("Print" "%p" TeX-run-command t t :help "Print the file")
+        ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
+        ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file")
+        ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
+        ("Check" "lacheck %s" TeX-run-compile nil
+         (latex-mode)
+         :help "Check LaTeX file for correctness")
+        ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
+        ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
+        ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
+        ("Other" "" TeX-run-command t t :help "Run an arbitrary command")))
 
 ;; The pretty cool auctex extension.
 (load "auctex.el" nil t t)
