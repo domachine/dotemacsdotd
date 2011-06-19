f504217c753632347a75866386c8f2d303a5457e
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-06-19 13:06:09 +0200

    Added keybinding for gnuplot-buffer-fn.

--- site-start.d/90keybindings.el
+++ site-start.d/90keybindings.el
@@ -13,6 +13,10 @@
 (global-set-key (kbd "C-c C-v") 'quit-window)
 
 (global-set-key [(f9)] 'gnuplot-make-buffer)
+(add-hook 'gnuplot-mode-hook
+          (lambda ()
+            (define-key gnuplot-mode-map
+              (kbd "C-c C-c") 'gnuplot-buffer-fn)))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
