f0bc70bd5cbd784c7777c8ae3695c9205ac724c9
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-06-20 21:39:31 +0200

    Fixed org heading insertion on console.

--- site-start.d/65org.el
+++ site-start.d/65org.el
@@ -50,3 +50,11 @@
                       ("\\.mm\\'" . default)
                       ("\\.x?html?\\'" . default)
                       ("\\.pdf\\'" . "DISPLAY=:0 xpdf %s")))
+
+(defun my-org-insert-heading (prefix)
+  (interactive "P")
+  (if prefix
+      (org-insert-heading-respect-content)
+    (org-insert-heading)))
+
+(define-key org-mode-map (kbd "M-<RET>") 'my-org-insert-heading)
