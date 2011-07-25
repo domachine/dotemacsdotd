8e8f7bf7bf3a9e1dcb7b0ae213a3442ebaf220ca
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-07-25 13:04:03 +0200

    Fixed haskell-mode path.

--- site-start.d/modes/09haskell.el
+++ site-start.d/modes/09haskell.el
@@ -1,2 +1,5 @@
 ;; == Haskell ==
-(load "haskell-mode/haskell-site-file")
+;;(load "haskell-mode/haskell-site-file")
+
+(autoload 'haskell-mode (concat user-emacs-directory
+                                "elisp/haskell-mode/haskell-mode.el") nil t)
