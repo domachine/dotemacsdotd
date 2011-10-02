98d70e6d7d303adcf281d71a5a057a4ea40a1f6c
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-10-02 18:04:11 +0200

    Some new ibuffer groups and org styles.

--- site-start.d/custom.el
+++ site-start.d/custom.el
@@ -4,7 +4,9 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
- '(font-lock-comment-face ((t (:foreground "red"))) nil "Highlight comments in source code correctly"))
+ '(font-lock-comment-face ((t (:foreground "red"))) nil "Highlight comments in source code correctly")
+ '(org-level-1 ((t (:inherit outline-1 :weight bold :height 1.5 :family "arial"))))
+ '(org-level-2 ((t (:inherit outline-2 :weight bold :height 1.2 :family "arial")))))
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,6 +18,7 @@
  '(TeX-parse-self t)
  '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "xpdf") (output-html "xdg-open"))))
  '(battery-mode-line-format "  [%b%p%%, %t]")
+ '(browse-url-browser-function (quote w3m-goto-url-new-session))
  '(c-basic-offset 4)
  '(c-default-style (quote ((c-mode . "k&r") (c++-mode . "k&r") (java-mode . "java") (awk-mode . "k&r"))))
  '(cal-tex-diary t)
@@ -29,11 +32,13 @@
  '(display-time-day-and-date t)
  '(display-time-format "%D %l:%M%p")
  '(display-time-mode t)
+ '(elim-directory "/home/dominik/.emacs.d/elim")
  '(erc-nick "domachine")
  '(find-file-wildcards t)
+ '(garak-hide-offline-buddies t)
  '(ibuffer-expert t)
  '(ibuffer-mode-hook (quote ((lambda nil (ibuffer-auto-mode 1) (ibuffer-switch-to-saved-filter-groups "default")))))
- '(ibuffer-saved-filter-groups (quote (("default" ("Emacs" (or (name . "^\\*scratch\\*$") (name . "^\\*Messages\\*$") (name . "^\\*Buffer List\\*$") (name . "^\\*Completions\\*$"))) ("Compilation" (or (name . "^\\*compilation\\*<?[0-9]*>?$") (name . "^\\*Compile\\-"))) ("Command Output" (name . "^.* output\\*$")) ("W3M" (or (mode . w3m-mode))) ("Shell" (or (mode . shell-mode) (mode . eshell-mode) (mode . term-mode))) ("Tramp" (or (name . "^\\*tramp\\/"))) ("Git" (or (name . "^\\*git\\-[a-zA-Z0-9\\-]+\\*<?[0-9]*>?$") (name . "^\\*vc\\*"))) ("Customize" (or (mode . Custom-mode))) ("Help" (or (mode . help-mode) (mode . Info-mode))) ("Planner" (or (name . "^\\*Calendar\\*$") (name . "^\\*Fancy Diary Entries\\*$") (mode . diary-mode) (mode . muse-mode) (mode . org-agenda-mode))) ("Mail" (or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode) (mode . rmail-mode) (name . "^\\.newsrc-dribble"))) ("Newsticker" (or (mode . newsticker-mode) (name . "^\\.newsticker-cache$"))) ("ERC" (or (mode . erc-mode))) ("BBDB" (or (mode . bbdb-mode) (name . "^\\.bbdb$"))) ("Calc" (or (mode . calc-mode) (mode . calc-trail-mode))) ("Java Project Buffer" (or (name . "project.el<?[0-9]*>?$"))) ("Man" (or (mode . Man-mode))) ("DocView" (or (mode . doc-view-mode))) ("NetworkUtil" (or (mode . net-utils-mode) (name . "^\\*DNS Lookup"))) ("Org" (or (mode . org-mode))) ("CMake" (or (mode . cmake-mode))) ("Python" (or (mode . python-mode))) ("Shell-script" (or (mode . shell-script-mode) (mode . sh-mode))) ("Perl" (or (mode . perl-mode) (mode . cperl-mode))) ("PHP" (or (name . "^.*\\.php$"))) ("HTML" (or (mode . html-mode))) ("YAML" (or (mode . yaml-mode))) ("C" (or (mode . c-mode))) ("C++" (or (mode . c++-mode))) ("Java" (or (mode . java-mode))) ("Javascript" (or (mode . js-mode))) ("TeX" (or (mode . latex-mode))) ("Lisp" (or (mode . emacs-lisp-mode) (mode . lisp-mode))) ("Dired" (mode . dired-mode))))))
+ '(ibuffer-saved-filter-groups (quote (("default" ("Emacs" (or (name . "^\\*scratch\\*$") (name . "^\\*Messages\\*$") (name . "^\\*Buffer List\\*$") (name . "^\\*Completions\\*$"))) ("Compilation" (or (name . "^\\*compilation\\*<?[0-9]*>?$") (name . "^\\*Compile\\-"))) ("Command Output" (name . "^.* output\\*$")) ("W3M" (or (mode . w3m-mode))) ("Shell" (or (mode . shell-mode) (mode . eshell-mode) (mode . term-mode))) ("Tramp" (or (name . "^\\*tramp\\/"))) ("Git" (or (name . "^\\*git\\-[a-zA-Z0-9\\-]+\\*<?[0-9]*>?$") (name . "^\\*vc\\*"))) ("Customize" (or (mode . Custom-mode))) ("Help" (or (mode . help-mode) (mode . Info-mode))) ("Planner" (or (name . "^\\*Calendar\\*$") (name . "^\\*Fancy Diary Entries\\*$") (mode . diary-mode) (mode . muse-mode) (mode . org-agenda-mode))) ("Mail" (or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode) (mode . rmail-mode) (name . "^\\.newsrc-dribble"))) ("Newsticker" (or (mode . newsticker-mode) (name . "^\\.newsticker-cache$"))) ("ERC" (or (mode . erc-mode))) ("Garak" (or (mode . garak-mode))) ("BBDB" (or (mode . bbdb-mode) (name . "^\\.bbdb$"))) ("Calc" (or (mode . calc-mode) (mode . calc-trail-mode))) ("Java Project Buffer" (or (name . "project.el<?[0-9]*>?$"))) ("Man" (or (mode . Man-mode))) ("DocView" (or (mode . doc-view-mode))) ("NetworkUtil" (or (mode . net-utils-mode) (name . "^\\*DNS Lookup"))) ("Org" (or (mode . org-mode))) ("CMake" (or (mode . cmake-mode))) ("Python" (or (mode . python-mode))) ("Shell-script" (or (mode . shell-script-mode) (mode . sh-mode))) ("Perl" (or (mode . perl-mode) (mode . cperl-mode))) ("PHP" (or (name . "^.*\\.php$"))) ("HTML" (or (mode . html-mode))) ("YAML" (or (mode . yaml-mode))) ("C" (or (mode . c-mode))) ("C++" (or (mode . c++-mode))) ("Java" (or (mode . java-mode))) ("Javascript" (or (mode . js-mode))) ("TeX" (or (mode . latex-mode))) ("Lisp" (or (mode . emacs-lisp-mode) (mode . lisp-mode))) ("Dired" (mode . dired-mode))))))
  '(ibuffer-saved-filters (quote (("asterisk" ((not name . "^\\*[^\\*]+\\*.*$"))) ("default" ((not name . "^\\.\\(newsticker-cache\\|bbdb\\)$"))) ("gnus" ((or (mode . message-mode) (mode . mail-mode) (mode . gnus-group-mode) (mode . gnus-summary-mode) (mode . gnus-article-mode)))) ("programming" ((or (mode . emacs-lisp-mode) (mode . cperl-mode) (mode . c-mode) (mode . java-mode) (mode . idl-mode) (mode . lisp-mode)))))))
  '(ibuffer-show-empty-filter-groups nil)
  '(indent-tabs-mode nil)
@@ -82,5 +87,5 @@
  '(shell-file-name "bash")
  '(tab-width 4)
  '(tool-bar-mode nil)
- '(w3m-default-display-inline-images t nil nil "Show images in w3m")
+ '(w3m-default-display-inline-images t)
  '(warning-suppress-types (quote ((undo discard-info)))))
