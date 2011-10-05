862ad1094f4035f931214b476f16d1a33034cfc6
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-10-05 12:37:53 +0200

    Some improvements of the site-utils.

--- elisp/site-utils.el
+++ elisp/site-utils.el
@@ -4,21 +4,26 @@
   "Executes body only if feature file was found in load-path.
 In fact a file of the form: <feature>.el or <feature>.elc"
   (let ((feature-test-symbol (intern (format "feature-exists-%s" (symbol-name feature)))))
-    (if (boundp feature-test-symbol)
-        (if (not (symbol-value feature-test-symbol))
-            nil
-          `(progn ,@body))
-      (let ((dir (gensym))
-            (src-file (gensym))
-            (c-file (gensym)))
-        `(catch 'break
-           (dolist (,dir load-path)
-             (let* ((,src-file (concat ,dir "/" (symbol-name ',feature) ".el"))
-                    (,c-file (concat ,src-file "c")))
-               (when (or (file-exists-p ,src-file) (file-exists-p ,c-file))
-                 (setf ,feature-test-symbol t)
-                 ,@body
-                 (throw 'break nil)))))))))
+    (if (or (featurep feature)
+            (and (boundp feature-test-symbol)
+                 (symbol-value feature-test-symbol)))
+        ;; This feature is already loaded or found.
+        `(progn ,@body)
+      (when (not (boundp feature-test-symbol))
+        ;; The feature wasn't searched before, so go and
+        ;; try to find it.
+
+        (let ((dir (gensym))
+              (src-file (gensym))
+              (c-file (gensym)))
+          `(catch 'break
+             (dolist (,dir load-path)
+               (let* ((,src-file (concat ,dir "/" (symbol-name ',feature) ".el"))
+                      (,c-file (concat ,src-file "c")))
+                 (when (or (file-exists-p ,src-file) (file-exists-p ,c-file))
+                   (setf ,feature-test-symbol t)
+                   ,@body
+                   (throw 'break nil))))))))))
 
 (defmacro load-feature (feature &rest initialization)
   "Loads the given feature and executes the initialization code
