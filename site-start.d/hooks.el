ce1dd3bc9d0e403b88b702582829ab388cf952fa
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-10-10 18:57:02 +0200

    AucTeX hook correction.

--- site-start.d/hooks.el
+++ site-start.d/hooks.el
@@ -1,7 +1,8 @@
 (add-hook 'ibuffer-mode-hook
           (lambda nil (ibuffer-auto-mode 1) (ibuffer-switch-to-saved-filter-groups "default")))
 
-(add-hook 'LaTeX-mode-hook #'TeX-PDF-mode)
+(do-if-feature-exists auctex
+                      (add-hook 'LaTeX-mode-hook #'TeX-PDF-mode))
 
 (setq c-mode-common-hook '((lambda nil (local-set-key (kbd "C-c C-c") 'compile))
                            (lambda nil (c-toggle-auto-state 1))))
