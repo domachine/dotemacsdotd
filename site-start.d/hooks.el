89dd87807b9d0b7a6d37db7e11a6a2228db3d8bd
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-10-30 22:53:40 +0100

    Added some hooks to fix issues of yasnippet in org-mode.

--- site-start.d/hooks.el
+++ site-start.d/hooks.el
@@ -28,3 +28,15 @@
 
 (do-if-feature-exists ajc-java-complete-config
                       (add-hook 'java-mode-hook #'ajc-java-complete-mode))
+
+;; Solve yasnippet vs. org-mode issue.
+(do-if-feature-exists yasnippet
+                      (defun yas/org-very-safe-expand ()
+                        (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+
+                      (add-hook 'org-mode-hook
+                                (lambda ()
+                                  (make-variable-buffer-local 'yas/trigger-key)
+                                  (setq yas/trigger-key [tab])
+                                  (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+                                  (define-key yas/keymap [tab] 'yas/next-field))))
