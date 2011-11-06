51425e73dd923bc61ff575f4cfbea9d4156d0634
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2011-11-06 16:37:49 +0100

    Removed foreground and background color from default face.

--- site-start.d/custom.el
+++ site-start.d/custom.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#042028" :foreground "#708183" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(font-lock-comment-face ((t (:foreground "red"))) nil "Highlight comments in source code correctly")
  '(org-level-1 ((t (:inherit outline-1 :weight bold :height 1.5 :family "arial"))))
  '(org-level-2 ((t (:inherit outline-2 :weight bold :height 1.2 :family "arial"))))
